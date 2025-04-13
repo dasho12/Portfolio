5687252c484df8817acb71a1e8891729ce9b5033
dasho <zeweg0319@gmail.com>
2025-04-14 00:36:20 +0800

    first commit

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,8 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
+import pkg from "@prisma/client";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/lib/auth";
 
+const { PrismaClient } = pkg;
 const prisma = new PrismaClient();
 
 // GET all contact requests (admin only)
